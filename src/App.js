bd166319e72e63ca4ee2d39120f78212c082d17e
ninaomar2021 <78866652+ninaomar2021@users.noreply.github.com>
2021-07-31 18:24:29 -0400

    Note taker app Module 4
    Note taking app will add a note

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import React, {useState} from 'react';
+import './style.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CreateArea from './components/CreateArea';
+import Note from './components/Note';
+
+function App(props) {
+  const[notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+      setNotes((prevValue) => {
+        return [...prevValue, newNote];
+      });
+    }
+
+    function deleteNotes(id) {
+        setNotes((preValue) => {
+            return [...preValue.filter((note, index) => index !== id)];
+        });
+    }
+      return(
+        <div className="App">
+          <Header />
+          <CreateArea onAdd={addNote} />
+          {notes.map((note, index) => (
+            <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNotes}
+          />
+          ))}
+         <Footer />
+        </div>
+      );
+}
+
+export default App;
